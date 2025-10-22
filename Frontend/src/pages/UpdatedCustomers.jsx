f5e65b0eea741cf3aa52ff253a5184a171ee0913
Agrim Gupta <agrim291104@gmail.com>
2025-10-23 04:50:06 +0530

    Change: fecting successful customer logic

--- Frontend/src/pages/UpdatedCustomers.jsx
+++ Frontend/src/pages/UpdatedCustomers.jsx
@@ -68,15 +68,15 @@ function UpdatedCustomers() {
                             <tbody className="bg-white divide-y divide-gray-200">
                                 {
                                     customers.map((customer) => (
-                                        customer.status === 'SUCCESSFUL' && (
+                                        customer.call_status === 'SUCCESSFUL' && (
                                             <tr key={customer.id}>
                                                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{customer.name}</td>
                                                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{customer.phone}</td>
                                                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">${customer.loanAmount}</td>
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{customer.message}</td>
+                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{customer.notes}</td>
                                                 <td className="px-6 py-4 whitespace-nowrap">
-                                                    <span className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${customer.status === 'Updated' ? 'bg-green-100 text-green-800' : 'bg-yellow-100 text-yellow-800'}`}>
-                                                        {customer.status}
+                                                    <span className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${customer.call_status === 'SUCCESSFUL' ? 'bg-green-100 text-green-800' : 'bg-yellow-100 text-yellow-800'}`}>
+                                                        {customer.call_status}
                                                     </span>
                                                 </td>
                                             </tr>
